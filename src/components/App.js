73e064b8ddbe03259f0008ed969bee4e064d999a
mellem <cph-ml474@cphbusiness.dk>
2018-11-19 19:34:57 +0100

    sidemenu

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../styles/App.css';
+import '../styles/styles.css';
 import TutorialPage from './TutorialPage';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import StartPage from './StartPage';
@@ -10,37 +10,46 @@ class App extends Component {
 
     constructor(props) {
         super(props);
+        // this.state = {
+        //     showMenu: false,
+        // }
     }
+
+    // changeColor() {
+    //     this.setState({showMenu: !this.state.showMenu})
+    // }
+
 
     render() {
 
+        let sidemenu_class = "show-sidemenu"
+        // let toggle_sidemenu_class = this.state.showMenu ? "toogle-hide-sidemenu" : "toogle-show-sidemenu";
+        // let btn_icon = this.state.showMenu ? <MenuIcon  width={50} fill={'black'} /> : <HideMenuIcon  width={50} fill={'black'} />;
+
         return (
             <Router basename={`${this.props.history.location.pathname}`}>
-                <div className="grid-container-frame">
-                    <div className="grid-container-main" >
-                        <div id='search-topbar' className="frame-grid-item">
-                            
-                        </div>
-                        
-                        <div id='main-topbar' className="frame-grid-item">
-                            <div id='logo-container'>
-                                <Link to='/'><button className='button-logo' type="submit" >mekkl.</button></Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="frame-grid-item">
-                        <Router basename={`${this.props.history.location.pathname}`}>
-                            <Switch>
-                                <Route exact path="/" component={StartPage} />
-                                <Route exact path="/tutorials*" component={TutorialPage} />
-                                <Route component={PageNotFound} />
-                            </Switch>
-                        </Router>
-                    </div >
+            <div className='page'>
+                
+                <div className='topmenu'>
+
                 </div >
+                <div className='content'>
+
+                </div >
+                <div className={sidemenu_class}>
+                    
+                </div >
+            </div>
             </Router>
         );
     }
 }
 
 export default App;
+
+
+{/* <div id='main-topbar' className="frame-grid-item">
+                            <div id='logo-container'>
+                                <Link to='/'><button className='button-logo' type="submit" >mekkl.</button></Link>
+                            </div>
+                        </div> */}
